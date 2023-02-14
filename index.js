e19371bfc7b5fd9ca5c499b6a0c28811b4d169ea
Casey <c.m.s.lee0207@gmail.com>
2023-02-14 11:34:20 -0500

    message

--- index.js
+++ index.js
@@ -12,14 +12,67 @@
 // I. Variables & Data Types
 // A. Q + A
 
+/*
+1. How to assign value to variable? // with assignment operator =
+2. How to change value of variable? // 
+3. How to assign existing variable a new variable? // with assignment operator =
+4. Definitions of declare, assign, and define // declare: using keywords to introduce ____; assign: used to give value to a variable; define: for expressions?
+5. What is pseudocoding and why is it useful? // Expressing in plain English the steps that are happening. It is useful for making sure you know where your code is going and that it is fulfilling its intended function.
+6. How much time should be spent thinking about how to solve a problem vs. actually typing in code? This is probably different depending on how different people think/work best, but, generally at least the same if not more time should be spent thinking about how to solve a problem than implementing since it likely saves time and energy for most to make sure their logic tracks before trying to define it in code.
+*/
+
 // B. Strings
+
+var firstVariable;
+firstVariable = "Hello World";
+firstVariable = 8;
+secondVariable = firstVariable;
+secondVariable = "Any String";
+console.log(firstVariable); // 8
+
+const yourName = "Casey"
+console.log(`Hello, my name is ${yourName}`);
+
+
 // C. Booleans
+
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = 'Kevin';
+
+console.log(a < b);
+console.log(c > d);
+console.log('Name' === 'Name');
+console.log(true || false);
+console.log(false && false && false && false && false || true);
+console.log(false === false);
+console.log(e === 'Kevin');
+console.log(a + b === c);
+console.log(a*a===d);
+console.log(48 !== '48');
+
 // D. The Farm
+
+var animal = "fox"
+if ( x = "fox"){
+	console.log("caw-shriek!");
+} else {
+	console.log("Hey! You're not a fox!");
+}
+
 // E. Driver's Ed
+
+var yourAge = 21
+
+if (yourAge >= 16){
+	console.log("Here are the keys!");
+} else if (yourAge < 16){
+	console.log("Sorry, you're too young.")
+}
+
 //_____________________________________________________________
-
-
-
 
 
 
