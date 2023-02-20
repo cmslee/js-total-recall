f0779eddafa59738b11ccc5bba7eb29fbaa78a55
Casey <c.m.s.lee0207@gmail.com>
2023-02-20 16:17:38 -0500

    created timmys story

--- day_3.js
+++ day_3.js
@@ -17,30 +17,105 @@ class Hamster {
       return this.price
     }
   }
-
-
-const hamster1 = new Hamster("carl") 
-
-hamster1.wheelRun()
 */
+
+// const hamster1 = new Hamster("carl") 
+// hamster1.wheelRun()
+
 
 
 // Person
 
+/*
 class Person {
-    constructor(name){
-        this.name = name
-        this.age = 0
-        this.height = 0
-        this.weight = 0
-        this.modd = 0
-        this.hamsters = []
-        this.bankAccount = 0
-    }
+  constructor(name) {
+    this.name = name
+    this.age = 0
+    this.height = 0
+    this.weight = 0
+    this.mood = 0
+    this.hamsters = []
+    this.bankAccount = 0
+  }
 
-    getName(){
-        return this.name
-    }
+  getName() {
+    return this.name
+  }
 
-    
+  getAge() {
+    return this.age
+  }
+
+  getWeight() {
+    return this.weight
+  }
+
+  greet() {
+    console.log(`Hello! I am ${this.name}.`)
+  }
+
+  eat() {
+    this.weight++;
+    this.mood++
+  }
+
+  exercise() {
+    this.weight--
+  }
+
+  ageUp() {
+    this.age++
+    this.height++
+    this.weight++
+    this.mood--
+    this.bankAccount += 10
+  }
+
+  buyHamster(hamster) {
+    this.hamsters.push(hamster)
+    this.mood += 10
+    this.bankAccount -= hamster.getPrice()
+  }
 }
+
+//1.
+const person1 = new Person("Timmy")
+//2.
+for(i=1; i<=5; i++){
+  person1.ageUp()
+}
+//3.
+for(i=0; i<5; i++){
+  person1.eat()
+}
+//4.
+for(i=0; i<5; i++){
+  person1.exercise()
+}
+//5.
+for(i=0; i<9; i++){
+  person1.ageUp()
+}
+//6. Create a hamster named "Gus"
+const hamster2 = new Hamster("Gus")
+//7. set Gus's ownder to the string "Timmy"
+hamster2.owner = "Timmy"
+//8. Have Timmy "buy" Gus
+person1.buyHamster(hamster2)
+//9. Age Timmy 15 years
+for(i=0; i<15; i++){
+  person1.ageUp()
+}
+//10. Have Timmy eat twice
+person1.eat()
+person1.eat()
+//11. Have Timmy exercise twice
+for(i=0; i<2;i++){
+  person1.exercise()
+}
+*/
+
+// console.log(hamster2)
+// console.log(person1)
+
+
